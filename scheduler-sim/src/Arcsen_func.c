c46fb48d47718fc98fe80a7cdf1bb62c0fa8742f
manchii <kaleb.23415@gmail.com>
2019-10-13 11:09:22 -0600

    arcsen aproximating arcsen

--- scheduler-sim/src/Arcsen_func.c
+++ scheduler-sim/src/Arcsen_func.c
@@ -3,46 +3,30 @@
 
 int factorial(int number){
   int factorial = 1;
-  if (number < 0){
-    if (number == 0){
-      return 1;
-    }
-    else{
-      return 1;
-    }
+  for(int i=1; i <= number; ++i){
+    factorial *= i;
   }
-  else{
-    for(int i=1; i <= number; ++i){
-      factorial *= i;
-    }
-    return factorial;
-  }
+  return factorial;
 }
 float arcsen(int n){
-  double temp1;
-  double temp2;
-  double temp3;
-  double temp4;
-  double var;
-  int i=0;
-  for(i; i <= n; ++i){
-    temp1 = pow(4,i) * pow(factorial(i),2) * (2*i + 1);
-    temp2 = factorial(2*i);
-    temp3 = temp2 / temp1;
-    temp4 = temp3 * pow (1,(2*i+1)) + var;
-    var = temp4;
-    printf("num ");
+  long double coeff=1.0;
+  long double result=0;
+  for(int i=1; i <= n; i++){
+    if(i%2==1){
+      result+=coeff/i;
+      coeff*=i;
+    }else
+      coeff/=i;
   }
-  //var = i;
-  return var;
+  return result;
 }
 
 int main(){
-  int n = 20;
+  int n = 100000;
   double temp4;
   temp4 = arcsen(n);
-  temp4 = 2 * (double)temp4;
-  printf("the results is: %0.10lf", temp4);
+  temp4 = 2.0 *temp4;
+  printf("the results is: %0.10lf\n", temp4);
   return 0;
 }
 // gcc Arcsen_func.c -o ArcSenProgram -lm
