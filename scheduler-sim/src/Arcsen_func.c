4d9eec08039faddd92ae7f2542a2f5b6b90d6c42
manchii <kaleb.23415@gmail.com>
2019-10-13 17:18:22 -0600

    Expropiación realizada. Falta sortear el ganador

--- scheduler-sim/src/Arcsen_func.c
+++ scheduler-sim/src/Arcsen_func.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <setjmp.h>
 
 
@@ -14,14 +16,17 @@ typedef struct Process{
   int quantum;
   bool suspended;
   bool finished;
+  long double coeff;
+  long double result;
+  int indx_term;
 }Process;
 
 double pi_approx_arcsen(Process *proc, Env_buf *scheduler_env){
-  long double coeff=2.0;
-  long double result=0.0;
-  int end=proc->workload*50;
-  int suspend=proc->quantum*50;
-  for(int i=1; i <= end; i++){
+  long double result = proc->result;
+  long double coeff = proc->coeff;
+  int end = proc->workload*50;
+  int suspend = proc->quantum*50;
+  for(int i=proc->indx_term; i <= end; i++){
     if(i%2==1){
       result+=coeff/i;
       coeff*=i;
@@ -30,22 +35,22 @@ double pi_approx_arcsen(Process *proc, Env_buf *scheduler_env){
 /*
 Suspend
 */
-    if(i%suspend!=0){
-      setjmp(proc->env);
-   }else{
-      proc->suspended=true;
-      printf("jmp: %d\n",i);
-      longjmp(*scheduler_env,0);
-    }
-    if(i%500==0)
-      printf("pid:%d, terms:%d ,approx:%0.10Lf\n",proc->pid,i,result);
+  if(i%500==0)
+    printf("pid:%d, terms:%d ,approx:%0.10Lf\n",proc->pid,i,result);
+  if(i%suspend==0 && i>0){
+    proc->indx_term=i+1;
+    proc->result=result;
+    proc->coeff=coeff;
+    proc->suspended=true;
+    longjmp(*scheduler_env,0);
+  }
 /*
 Suspend
 */
   }
   proc->suspended=false;
   proc->finished=true;
-  return result;
+  return proc->result;
 }
 
 int choose_winner(Process *p,int size){
@@ -67,11 +72,7 @@ int scheduler(Process *p, int size){
   setjmp(scheduler_env);
   int pid_winner = choose_winner(p,size);
   printf("winner: pid%d\n",pid_winner);
-  if(!p[pid_winner].suspended){
-    pi_approx_arcsen(&p[pid_winner],&scheduler_env);
-    printf("finished: %d\n",p[pid_winner].finished);
-  }else
-    longjmp(p[pid_winner].env,0);
+  pi_approx_arcsen(&p[pid_winner],&scheduler_env);
   if(!processes_are_finished(p,size)){
     longjmp(scheduler_env,0);
   }
@@ -79,16 +80,20 @@ int scheduler(Process *p, int size){
 }
 
 int main(){
-  Process p[2];
+  Process *p=(Process*)malloc(2*sizeof(Process));
   for(int i=0;i<2;i++){
     p[i].pid=i;
-    p[i].workload=1000;
+    p[i].workload=20;
     p[i].tickets=1;
     p[i].suspended=false;
-    p[i].quantum=10000;
+    p[i].quantum=1;
+    p[i].coeff=2.0;
+    p[i].result=0;
     p[i].finished=false;
+    p[i].indx_term=1;
   }
   scheduler(p,2);
+  free(p);
   return 0;
 }
 // gcc Arcsen_func.c -o ArcSenProgram -lm
