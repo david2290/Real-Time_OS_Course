07a9629603c3e16695e6bc44c7a037ffd3a6c07a
david2290 <djosera22@gmail.com>
2019-10-18 10:10:09 -0600

    Integration Beta1.0
    need to be added: File IO and multi-progress bar

--- scheduler-sim/src/Arcsen_func.c
+++ scheduler-sim/src/Arcsen_func.c
@@ -9,11 +9,33 @@
 #include <unistd.h>
 #include <setjmp.h>
 
-#define NUM_PROCESSES 5
+#include <gtk/gtk.h>
+#include <gtk/gtkx.h>
+#include <sys/types.h>
+#include <string.h>
+#include <ctype.h>
 
-typedef sigjmp_buf Env_buf;
+#define NUM_PROCESSES 5  // CAmbiar por los procesos q estan en el archivo
+//***************** GTK ***************************
+//gcc -Wno-format -o TestUI TestGladeINtegration.c -Wno-deprecated-declarations -Wno-format-security -lm `pkg-config --cflags --libs gtk+-3.0` -export-dynamic
 
-typedef struct Process{
+GtkWidget     *window;
+GtkWidget     *Gtk_Fixed;
+GtkWidget     *progress_bar1;
+GtkWidget     *progress_barArreglo[25]; // OJO
+GtkWidget     *progress_bar2;
+GtkWidget     *button;
+GtkWidget     *label;
+GtkWidget     *LabelArreglo[25];        // OJO
+
+GtkBuilder    *builder;
+
+
+
+
+typedef sigjmp_buf Env_buf;   // para guardar el contexto
+
+typedef struct Process{   //
   Env_buf env;
   int pid;
   int workload;
@@ -41,21 +63,44 @@ void config_timeout(){signal(SIGALRM,sig_timeout);}
 
 void start_timeout_timer(int quantum){ualarm(quantum,0);}
 
+void start_timeout_timer(int quantum){
+  timeout=false;
+  ualarm(quantum,0);
+}
+
+gdouble fraction = 0.0;
+gdouble test = 0.0;
+gchar *display;
+
 double pi_approx_arcsen(Process *proc, Env_buf *scheduler_env){
+  char tmp[20] = "6";// BORRAR DESPUES
+  gdouble fraction = gtk_progress_bar_get_fraction (GTK_PROGRESS_BAR (progress_bar1));
+
+
   long double result = proc->result;
   long double coeff = proc->coeff;
   int end = proc->workload*50;
+
   for(int i=proc->indx_term; i <= end; i++){
     if(i%2==1){
       result+=coeff/i;
       coeff*=i;
+      //test = 1.0/end;
+      //display = g_strdup_printf("%d", i);
+      //gtk_label_set_text (GTK_LABEL(label), display);
+      //gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (progress_bar1), test*i);
+
     }else
       coeff/=i;
 /*
 Suspend
 */
   if(i%500==0)//cada 500 terminos hace un print
-    printf("pid:%d, terms:%d ,approx:%0.10Lf\n",proc->pid,i,result); //Dif las barras
+    //printf("pid:%d, terms:%d ,approx:%0.10Lf\n",proc->pid,i,result); //Dif las barras
+    test = 1.0/end;
+    display = g_strdup_printf("%d", i);
+    gtk_label_set_text (GTK_LABEL(label), display);
+    gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (progress_bar1), test*i);
   // Se podria agregar q actualice la barra
 
   if(timeout){
@@ -89,7 +134,7 @@ choose_winner(Lottery_task *lt){
 
 void
 update_arrival(
-    Lottery_task *not_ready_queue,
+    Lottery_task *not_ready_queue,                   // Preguntar a kaleb***********
     Lottery_task *ready_queue)
 {
   static time_t init_time = 0;
@@ -125,11 +170,11 @@ update_process_finished(
 
 int lottery_scheduler(Lottery_task *lt){
   int total_processes = g_list_length(lt->lst);
-  Lottery_task not_ready_queue = *lt;
-  Lottery_task ready_queue = {.lst=NULL, .quantum=lt->quantum, .total_tickets=0};
-  Lottery_task finished_queue = {.lst=NULL, .quantum=lt->quantum, .total_tickets=0};
+  Lottery_task not_ready_queue = *lt;                                                //
+  Lottery_task ready_queue = {.lst=NULL, .quantum=lt->quantum, .total_tickets=0};    // se definen los estados de los procesos
+  Lottery_task finished_queue = {.lst=NULL, .quantum=lt->quantum, .total_tickets=0}; //
   config_timeout();
-  while(g_list_length(finished_queue.lst)<total_processes){
+  while(g_list_length(finished_queue.lst)<total_processes){   //mientras no haya terminado con todos los procesos
     Env_buf scheduler_env;
     sigsetjmp(scheduler_env,1);
     update_arrival(&not_ready_queue,&ready_queue);
@@ -172,10 +217,41 @@ void free_lottery_task(Lottery_task* lt){
   g_list_free_full(lt->lst,g_free);
 }
 
-int main(){
+
+
+// tRIGGER PARA INICIAR LOS PROCESOS
+void onButton (GtkButton *b){
   Lottery_task lt = create_lottery_task(NUM_PROCESSES);
   lottery_scheduler(&lt);
   free_lottery_task(&lt);
   return 0;
 }
+
+int main(int argc, char *argv[]) {
+  gtk_init(&argc, &argv);
+//---------------------------------
+// XML COnnection
+// ---------------------------------
+  builder = gtk_builder_new_from_file ("InterfaceGlade.glade");
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  gtk_builder_connect_signals(builder, NULL);
+
+// Hacer un arreglo progress_bars tomando en cuenta los labels
+  Gtk_Fixed = GTK_WIDGET(gtk_builder_get_object(builder, "GtkFixed"));
+  progress_bar1 = GTK_WIDGET(gtk_builder_get_object(builder, "process1"));
+  progress_bar2 = GTK_WIDGET(gtk_builder_get_object(builder, "process2"));
+  label = GTK_WIDGET(gtk_builder_get_object(builder, "label"));
+
+  button = GTK_WIDGET(gtk_builder_get_object(builder, "button"));
+  gtk_widget_show (window);
+  gtk_main();
+
+  return EXIT_SUCCESS;
+}
+
+
+
+
+
 // gcc  Arcsen_func.c -o ArcSenProgram -lm -fsanitize=address -fno-omit-frame-pointer `pkg-config --cflags --libs glib-2.0` -g -Wall
