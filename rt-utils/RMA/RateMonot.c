7b0c10d54c30208c50290fbbff93f4a634ca07bd
manchii <kaleb.23415@gmail.com>
2019-11-23 23:04:13 -0600

    refactor

--- rt-utils/RMA/RateMonot.c
+++ rt-utils/RMA/RateMonot.c
@@ -1,145 +1,131 @@
 #include<stdio.h>
+#include<stdbool.h>
+#include<gmodule.h>
 
-int Ps[2][100],realTimeCapacity[2][100], processesNumber = 0, LCM;
-void getSystem()
-{
-    FILE *InputFile;
+typedef struct Process{
+	int compute_time;
+	int period_time;
+} Process;
 
-	InputFile = fopen("sys.txt", "r");
-	while(!feof(InputFile))
-	{
-        fscanf(InputFile, "%d %d", &Ps[0][processesNumber], &Ps[1][processesNumber]);
+int realTimeCapacity[2][100];
+void createSystemFromFile(GArray *readyQueue){
+    FILE *InputFile = fopen("sys.txt", "r");
+	int processesNumber = 0;
+	while(!feof(InputFile)){
+		Process readProcess={0,0};
+        fscanf(InputFile, "%d %d",
+			&readProcess.compute_time,
+			&readProcess.period_time);
+		g_array_append_val(readyQueue, readProcess);
         processesNumber++;
     }
+}
 
+int calc_greatest_common_multiple(int a,int b){
+	if (b == 0) return a;
+	return calc_greatest_common_multiple(b, a % b);
 }
-int lcm(int t[], int tSize)
-{
-    int initialArray[100];
-    for(int i=0; i<tSize; i++)
-    {
-            initialArray[i] = t[i];
-    }
-    int index,m,x,b = 1;
-    while(b == 1)
-    {
-          b = 0;
-          x = t[0];
-          m = t[0];
-          index = 0;
-          for(int i=0 ; i<tSize; i++)
-          {
-                  if(x != t[i])
-                  {
-                       b = 1;
-                  }
-                  if(m > t[i])
-                  {
-                       m = t[i];
-                       index = i;
-                  }
-          }
-          if(b==1)
-          {
-                t[index] = t[index] + initialArray[index];
-          }
-    }
-    return t[0];
+
+int calc_least_common_multiple(GArray *readyQueue){
+	int length = (*readyQueue).len;
+	int result = g_array_index(readyQueue,Process,0).period_time;
+    for (int i = 1; i < length; i++){
+		int i_number = g_array_index(readyQueue,Process,i).period_time;
+		int gcd = calc_greatest_common_multiple(i_number, result);
+		result = (i_number*result)/gcd;
+	}
+    return result;
 }
-int priorProcess()
-{
+
+
+int RM_policy(GArray *simulationState, int lcm){
     // this function returns the id of the prior process and returns -1 if no process is ready to execute
-    int prior = -1, lessPeriode = LCM;
-    for(int i=0; i<processesNumber; i++)
-    {
-            if(realTimeCapacity[0][i] != 0)
-            {
-               if(lessPeriode > realTimeCapacity[1][i])
-               {
-                              prior = i;
-                              lessPeriode = realTimeCapacity[1][i];
-               }
-            }
+	const int numberOfProcesses = (*simulationState).len;
+    int prior = -1, lessPeriode = lcm;
+    for(int i=0; i<numberOfProcesses; i++){
+		int compute_time = g_array_index(simulationState,Process,i).compute_time;
+		int period_time = g_array_index(simulationState,Process,i).period_time;
+		if(compute_time > 0){
+			if(lessPeriode > period_time){
+				prior = i;
+				lessPeriode = period_time;
+			}
+		}
     }
     return prior;
 }
 
-
-void schedule()
-{
-     int t,prior;
-      ////////////////////////////////////////////intializing real time Capacity/restTime
-      for(int i=0; i<processesNumber; i++)
-      {
-              realTimeCapacity[0][i] = Ps[0][i];
-              realTimeCapacity[1][i] = Ps[1][i];
-      }
-
-      for(t=0; t<LCM; t++)
-      {
-               // schedulability necessery condition
-              for(int i= 0; i< processesNumber; i++)
-              {
-                      if(realTimeCapacity[0][i] > realTimeCapacity[1][i])
-                      {
-                           printf("\nthe system is not schedulable because the task %d cant finish its job in its specified period",i);
-                           return;
-                      }
-              }
-              /// fetch if periode finished without Pi finishes its work
-              prior = priorProcess();
-              if(prior != -1)
-              {
-                       printf("\n(%d-%d)| t%d",t,t+1, prior);
-              }
-              else
-              {
-                  printf("\n(%d-%d)",t,t+1);
-              }
-
-              realTimeCapacity[0][prior]--;
-              ////////////////////////// update period time
-              for(int i= 0; i< processesNumber; i++)
-              {
-                      realTimeCapacity[1][i]--;
-                      if(realTimeCapacity[1][i] == 0)
-                      {
-                                                realTimeCapacity[0][i] = Ps[0][i];
-                                                realTimeCapacity[1][i] = Ps[1][i];
-                      }
-              }
-
-
-
-      }
-}
-int main()
-{
-      int x,help[100];
-      getSystem();
-      //schedulability...
-      float sum=0;
-      for(int i=0; i< processesNumber; i++)
-      {
-            sum = sum +  ((float)Ps[0][i]/(float)Ps[1][i]);
-      }
-      if(sum>1)
-      {
-               printf("\nthis system isn't schedulable because\nthe sum of Ei/Pi = %f > 1\n",sum);
-
-      }
-      else
-      {
-             /////////////////////////////////////////// getting LCM...
-             for(int i=0; i<processesNumber; i++)
-             {
-                     help[i] = Ps[1][i];
-             }
-             LCM = lcm(help, processesNumber);
-             /////////////////////////////////////////// start scheduling
-             schedule();
-      }
-      scanf("%d",&x);
+void refresh_deadlines_accordingly(GArray *simulationState, GArray *readyQueue){
+	for(int i=0; i<(*simulationState).len; i++){
+		g_array_index(simulationState,Process,i).period_time--;
+		if(g_array_index(simulationState,Process,i).period_time == 0){
+			g_array_index(simulationState,Process,i) = g_array_index(readyQueue,Process,i);
+		}
+	}
 }
 
-// gcc RateMonot.c -o RMA
+bool found_schedule_error(GArray *simulationState){
+	for(int i= 0; i< (*simulationState).len; i++){
+		if(g_array_index(simulationState,Process,i).compute_time
+			> g_array_index(simulationState,Process,i).period_time){
+			printf("\nthe system is not schedulable because the task %d cant finish its job in its specified period",i);
+			return true;
+		}
+	}
+	return false;
+}
+
+void print_schedule_step_output(GArray *simulationState, int running_process_index, int t){
+	if(running_process_index != -1){
+		printf("\n(%d-%d)| t%d",t,t+1, running_process_index);
+		g_array_index(simulationState,Process,running_process_index).compute_time--;
+	}
+	else{
+		printf("\n(%d-%d)",t,t+1);
+	}
+}
+
+void schedule(GArray *readyQueue){
+	int numberOfProcesses = (*readyQueue).len;
+	int lcm = calc_least_common_multiple(readyQueue);
+	GArray *simulationState = g_array_new(FALSE,FALSE,sizeof(Process));
+	g_array_append_vals(simulationState,(*readyQueue).data,(*readyQueue).len);
+	int running_process_index;
+    for(int t=0; t<lcm; t++){
+		if(found_schedule_error(simulationState)) return;
+		running_process_index = RM_policy(simulationState,lcm);
+		print_schedule_step_output(simulationState,running_process_index,t);
+		refresh_deadlines_accordingly(simulationState,readyQueue);
+	}
+	g_array_free(simulationState,TRUE);
+}
+
+float calc_cpu_utilization(GArray *readyQueue){
+	double utilization_acc=0.0f;
+	int numberOfProcesses = (*readyQueue).len;
+	for(int i=0; i < numberOfProcesses; i++){
+		double compute_time = g_array_index(readyQueue,Process,i).compute_time;
+		double period_time = g_array_index(readyQueue,Process,i).period_time;
+		utilization_acc += compute_time/period_time;
+	}
+	float utilization = utilization_acc;
+	return utilization;
+}
+
+int main(){
+	GArray *readyQueue = g_array_new (FALSE, FALSE, sizeof(Process));
+    int x,help[100];
+    createSystemFromFile(readyQueue);
+    //schedulability...
+	float utilization = calc_cpu_utilization(readyQueue);
+	if(utilization>1){
+		printf("\nthis system isn't schedulable because\nthe sum of Ei/Pi = %f > 1\n",utilization);
+	}
+	else{
+	    schedule(readyQueue);
+	}
+	printf("End of the simulation\n");
+	g_array_free(readyQueue, TRUE);
+}
+
+// gcc RateMonot.c -o RMA `pkg-config --cflags --libs glib-2.0` `pkg-config --cflags gtk+-3.0` `pkg-config --libs gtk+-3.0`
