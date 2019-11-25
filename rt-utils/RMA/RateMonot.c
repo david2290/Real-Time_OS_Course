98967742dbd683a9e40a38118d3e7d237c3ec901
manchii <kaleb.23415@gmail.com>
2019-11-24 19:18:17 -0600

    added edf and rm

--- rt-utils/RMA/RateMonot.c
+++ rt-utils/RMA/RateMonot.c
@@ -10,7 +10,7 @@ typedef struct Process{
 void createSystemFromFile(GArray *);
 int calc_greatest_common_multiple(int,int);
 int calc_least_common_multiple(GArray *);
-int RM_policy(GArray *, int);
+int RM_policy(GArray *, GArray *, int);
 void refresh_deadlines_accordingly(GArray *, GArray *);
 bool found_schedule_error(GArray *);
 void print_schedule_step_output(GArray *, int, int);
@@ -25,20 +25,19 @@ int main(){
 		printf("\nthis system isn't schedulable because\nthe sum of Ei/Pi = %f > 1\n",utilization);
 	else
 	    schedule(readyQueue);
-	printf("End of the simulation\n");
+	printf("\n");
 	g_array_free(readyQueue, TRUE);
 }
 
 void createSystemFromFile(GArray *readyQueue){
     FILE *InputFile = fopen("sys.txt", "r");
-	int processesNumber = 0;
 	while(!feof(InputFile)){
 		Process readProcess={0,0};
         fscanf(InputFile, "%d %d",
 			&readProcess.compute_time,
 			&readProcess.period_time);
-		g_array_append_val(readyQueue, readProcess);
-        processesNumber++;
+		if(readProcess.period_time>0)
+			g_array_append_val(readyQueue, readProcess);
     }
 }
 
@@ -50,7 +49,7 @@ int calc_greatest_common_multiple(int a,int b){
 int calc_least_common_multiple(GArray *readyQueue){
 	int length = (*readyQueue).len;
 	int result = g_array_index(readyQueue,Process,0).period_time;
-    for (int i = 1; i < length; i++){
+    for (int i = 0; i < length; i++){
 		int i_number = g_array_index(readyQueue,Process,i).period_time;
 		int gcd = calc_greatest_common_multiple(i_number, result);
 		result = (i_number*result)/gcd;
@@ -58,8 +57,24 @@ int calc_least_common_multiple(GArray *readyQueue){
     return result;
 }
 
+int RM_policy(GArray *simulationState, GArray *readyQueue, int lcm){
+    // this function returns the id of the prior process and returns -1 if no process is ready to execute
+	const int numberOfProcesses = (*simulationState).len;
+    int prior = -1, lessPeriode = lcm;
+    for(int i=0; i<numberOfProcesses; i++){
+		int compute_time = g_array_index(simulationState,Process,i).compute_time;
+		int period_time = g_array_index(readyQueue,Process,i).period_time;
+		if(compute_time > 0){
+			if(lessPeriode >= period_time){
+				prior = i;
+				lessPeriode = period_time;
+			}
+		}
+    }
+    return prior;
+}
 
-int RM_policy(GArray *simulationState, int lcm){
+int EDF_policy(GArray *simulationState, int lcm){
     // this function returns the id of the prior process and returns -1 if no process is ready to execute
 	const int numberOfProcesses = (*simulationState).len;
     int prior = -1, lessPeriode = lcm;
@@ -67,7 +82,7 @@ int RM_policy(GArray *simulationState, int lcm){
 		int compute_time = g_array_index(simulationState,Process,i).compute_time;
 		int period_time = g_array_index(simulationState,Process,i).period_time;
 		if(compute_time > 0){
-			if(lessPeriode > period_time){
+			if(lessPeriode >= period_time){
 				prior = i;
 				lessPeriode = period_time;
 			}
@@ -79,7 +94,8 @@ int RM_policy(GArray *simulationState, int lcm){
 void refresh_deadlines_accordingly(GArray *simulationState, GArray *readyQueue){
 	for(int i=0; i<(*simulationState).len; i++){
 		g_array_index(simulationState,Process,i).period_time--;
-		if(g_array_index(simulationState,Process,i).period_time == 0){
+		if(g_array_index(simulationState,Process,i).period_time == 0 &&
+			g_array_index(simulationState,Process,i).compute_time == 0){
 			g_array_index(simulationState,Process,i) = g_array_index(readyQueue,Process,i);
 		}
 	}
@@ -87,8 +103,10 @@ void refresh_deadlines_accordingly(GArray *simulationState, GArray *readyQueue){
 
 bool found_schedule_error(GArray *simulationState){
 	for(int i= 0; i< (*simulationState).len; i++){
-		if(g_array_index(simulationState,Process,i).compute_time
-			> g_array_index(simulationState,Process,i).period_time){
+		int compute_time = g_array_index(simulationState,Process,i).compute_time;
+		int period_time = g_array_index(simulationState,Process,i).period_time;
+		printf(" c:%d d:%d ", compute_time, period_time);
+		if(compute_time > period_time){
 			printf("\nthe system is not schedulable because the task %d cant finish its job in its specified period",i);
 			return true;
 		}
@@ -114,7 +132,7 @@ void schedule(GArray *readyQueue){
 	int running_process_index;
     for(int t=0; t<lcm; t++){
 		if(found_schedule_error(simulationState)) return;
-		running_process_index = RM_policy(simulationState,lcm);
+		running_process_index = RM_policy(simulationState,readyQueue,lcm);
 		print_schedule_step_output(simulationState,running_process_index,t);
 		refresh_deadlines_accordingly(simulationState,readyQueue);
 	}
