077d5a60f4d6ea6ac786c920c801f21d119b5470
manchii <kaleb.23415@gmail.com>
2019-11-26 19:00:55 -0600

    merge

--- rt-utils/src/main.c
+++ rt-utils/src/main.c
@@ -1,10 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "latex_tools.h"
+#include "schedule-utils.h"
 
-int main(int argc, char** argv){
+void print_trace(SC_SimTrace *struct_trace_ptr){
+	int len = struct_trace_ptr->trace->len;
+	if(struct_trace_ptr->utilization <= 1){
+		for(int i=0; i<len ; i++){
+			int running_process_index = g_array_index(struct_trace_ptr->trace,int,i);
+			if(running_process_index != -1){
+				printf("\n(%d-%d)| t%d",i,i+1, running_process_index);
+			}
+			else{
+				printf("\n(%d-%d)",i,i+1);
+			}
+		}printf("\n");
+		if(struct_trace_ptr->error){
+			int incomplete_task_id = struct_trace_ptr->incomplete_task_id;
+			printf("\nthe system is not schedulable because the task %d cant finish its job in its specified period\n",incomplete_task_id);
+		}
+	}else{
+		printf("\nthis system isn't schedulable because\nthe sum of Ei/Pi = %f > 1\n",struct_trace_ptr->utilization);
+	}
+}
+
+void plot_trace_pdf(){
 	RT_create_from_base_file();
 	RT_export_project();
 	RT_open_window();
+}
+
+void run_simulation(SC_SimTrace *struct_trace_ptr, SC_Policy schedulable_policy){
+	GArray *readyQueue = g_array_new (FALSE, FALSE, sizeof(SC_Process));
+    SC_createSystemFromFile(readyQueue);
+	float utilization = SC_calc_cpu_utilization(readyQueue);
+	struct_trace_ptr->utilization=utilization;
+	if(utilization>1){
+		struct_trace_ptr->error=true;
+	}else{
+		SC_schedule(readyQueue,schedulable_policy,struct_trace_ptr);
+	}
+	g_array_free(readyQueue, TRUE);
+}
+
+int main(int argc, char** argv){
+	SC_SimTrace sim_trace = {NULL, false, 0, 1};
+	sim_trace.trace = g_array_new(FALSE,FALSE,sizeof(int));
+	SC_Policy schedulable_policy = SC_RM_policy;
+	run_simulation(&sim_trace,schedulable_policy);
+	print_trace(&sim_trace);
+	g_array_free(sim_trace.trace,TRUE);
     return 0;
 }
---
+++ rt-utils/src/schedule-utils.h
@@ -0,0 +1,28 @@
+#include<stdbool.h>
+#include<gmodule.h>
+
+typedef struct SC_Process{
+	int compute_time;
+	int period_time;
+} SC_Process;
+
+typedef struct SC_SimTrace{
+	GArray *trace;
+	int error;
+	int incomplete_task_id;
+	float utilization;
+} SC_SimTrace;
+
+typedef int (*SC_Policy)(GArray*, GArray*, int);
+
+void SC_createSystemFromFile(GArray *);
+int SC_calc_greatest_common_multiple(int,int);
+int SC_calc_least_common_multiple(GArray *);
+int SC_RM_policy(GArray *, GArray *, int);
+int SC_EDF_policy(GArray *, GArray *, int);
+int SC_LLS_policy(GArray *, GArray *, int);
+void SC_refresh_deadlines_accordingly(GArray *, GArray *);
+void SC_found_schedule_error(GArray *, SC_SimTrace*);
+void SC_run_step(GArray *, int, int, SC_SimTrace* );
+void SC_schedule(GArray *,SC_Policy, SC_SimTrace*);
+float SC_calc_cpu_utilization(GArray *);
