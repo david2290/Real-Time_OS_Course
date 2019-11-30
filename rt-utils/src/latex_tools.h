358f3c20cf3981de476823b67bae447258af2f6c
manchii <kaleb.23415@gmail.com>
2019-11-29 23:57:16 -0600

    suibs

--- rt-utils/src/latex_tools.h
+++ rt-utils/src/latex_tools.h
@@ -212,6 +212,7 @@ void RT_print_trace_mixed(GArray *array_trace_ptr, FILE* f){
 	number_of_frames = (g_array_index(array_trace_ptr,SC_SimTrace,0).trace->len%40==0)?number_of_frames-1:number_of_frames;
 	for(int scheduler_idx=1; scheduler_idx < array_trace_ptr->len; scheduler_idx++){
 		number_of_frames = (g_array_index(array_trace_ptr,SC_SimTrace,scheduler_idx).trace->len / 40)+1;
+		number_of_frames = (g_array_index(array_trace_ptr,SC_SimTrace,0).trace->len%40==0)?number_of_frames-1:number_of_frames;
 	}
 	for(int frame_idx=0; frame_idx < number_of_frames; frame_idx++){
 		fprintf(f, "\\begin{frame}\n");
